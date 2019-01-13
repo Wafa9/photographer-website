f9205c86d80d7d40b7fe59914b88a622ebc546d7
Wafa9 <wafa.a.t14@gmail.com>
2019-01-13 03:12:04 +0300

    f

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 $(document).ready(function() {
   $("ul.tabs").tabs();
 });
 $(document).ready(function() {
   $("ul.tabs").tabs("select_tab", "tab_id");
 });
-=======
-function materialize() {
-  $(".dropdown-trigger").dropdown();
-  $(".sidenav").sidenav();
-}
-
-$(document).ready(materialize);
->>>>>>> d63603eb84a0dd7d10fa758a19b4b4bb257d57ff
